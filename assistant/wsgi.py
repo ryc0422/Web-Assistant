3be8fc76d5114ccfa46615e79b6221fac4cacf2b
Tiffjuyen <Tiffjuyen@Tiffjuyende-MacBook.local>
2021-10-14 00:06:00 +0800

    restore wsgi file

--- assistant/wsgi.py
+++ assistant/wsgi.py
@@ -8,17 +8,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
+
 from django.core.wsgi import get_wsgi_application
-# add your project directory to the sys.path
-project_home = u'home/ryc0422/assistant'
-if project_home not in sys.path:
-    sys.path.insert(0, project_home)
 
-# set enviroment variable to tell django where your settings.py is
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assistant.settings')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'assistant.settings'
-
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assistant.settings')
 
 application = get_wsgi_application()
